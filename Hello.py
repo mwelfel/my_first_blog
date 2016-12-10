a6770522db04816d3fc8692330dc6f2c8ba1f899
Marta Welfel <martawelfel@MacBook-Air-Marta.local>
2016-12-10 13:02:24 +0100

    my Django Girls app, first commit

---
+++ Hello.py
@@ -0,0 +1,8 @@
+
+dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+def hej(imie):
+    print('Hej ' + imie + '!')
+for imie in dziewczyny:
+    hej(imie)
+for i in range (1, 6):
+    print(i)
